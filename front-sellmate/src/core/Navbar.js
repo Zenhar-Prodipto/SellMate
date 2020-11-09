21dcfd6b8c101c62a0d8916e7aa42cd062a1e424
Zenhar-Prodipto <zenhar.mz@gmail.com>
2020-11-09 21:13:58 +0600

    Added Signout

--- front-sellmate/src/core/Navbar.js
+++ front-sellmate/src/core/Navbar.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -17,24 +18,45 @@ const Navbar = ({ history }) => (
           Home
         </Link>
       </li>
-      <li className="nav-items">
-        <Link
-          className="nav-link"
-          style={isActive(history, "/signin")}
-          to="/signin"
-        >
-          SignIn
-        </Link>
-      </li>
-      <li className="nav-items">
-        <Link
-          className="nav-link"
-          style={isActive(history, "/signup")}
-          to="/signup"
-        >
-          SignUp
-        </Link>
-      </li>
+      {!isAuthenticated() && (
+        <Fragment>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              style={isActive(history, "/signin")}
+              to="/signin"
+            >
+              Signin
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              style={isActive(history, "/signup")}
+              to="/signup"
+            >
+              Signup
+            </Link>
+          </li>
+        </Fragment>
+      )}
+
+      {isAuthenticated() && (
+        <li className="nav-items">
+          <span
+            className="nav-link"
+            style={{ cursor: "pointer", color: "white" }}
+            onClick={() =>
+              signout(() => {
+                history.push("/");
+              })
+            }
+          >
+            signout
+          </span>
+        </li>
+      )}
     </ul>
   </div>
 );
--- front-sellmate/src/user/Signin.js
+++ front-sellmate/src/user/Signin.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Layout from "../core/Layout";
 import { API } from "../config";
 import { Link, Redirect } from "react-router-dom";
-import { signin } from "../auth";
+import { signin, authenticate } from "../auth";
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -26,9 +26,11 @@ const Signin = () => {
       if (data.error) {
         setValues({ ...values, error: data.error, loading: false });
       } else {
-        setValues({
-          ...values,
-          redirectToReferrer: true,
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            redirectToReferrer: true,
+          });
         });
       }
     });
