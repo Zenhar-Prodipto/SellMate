ac360c30924d4d32bfa686cd3e8bfad9d79c3dbc
Zenhar-Prodipto <zenhar.mz@gmail.com>
2020-11-11 21:18:12 +0600

    minor changes to category db

--- SellMate/models/category.js
+++ SellMate/models/category.js
@@ -6,7 +6,8 @@ const categorySchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      maxlength: 2000,
+      maxlength: 40,
+      unique: true,
     },
   },
   { timestamps: true }
