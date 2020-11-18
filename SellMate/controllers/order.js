900390f6033a84ab1111b59d69b7a91278fdbb5a
Zenhar-Prodipto <zenhar.mz@gmail.com>
2020-11-18 18:19:44 +0600

    Completed the order process

--- SellMate/controllers/order.js
+++ SellMate/controllers/order.js
@@ -2,6 +2,20 @@ const { order, cartItem, Order } = require("../models/order");
 const { errorHandler } = require("../helpers/dbErrorHandlers");
 const { json } = require("express");
 
+exports.orderById = (req, res, id, next) => {
+  Order.findById(id)
+    .populate("products.product", "name price")
+    .exec((err, order) => {
+      if (err || !order) {
+        return res.status(400).json({
+          error: errorHandler(error),
+        });
+      }
+      req.order = order;
+      next();
+    });
+};
+
 exports.create = (req, res) => {
   req.body.order.user = req.user; //req.user is req.profile basically
 
@@ -27,6 +41,24 @@ exports.listOrders = (req, res) => {
           error: errorHandler(error),
         });
       }
+      res.json(orders);
     });
-  res.json(orders);
+};
+
+exports.getStatusValues = (req, res) => {
+  res.json(Order.schema.path("status").enumValues);
+};
+
+exports.updateOrderStatus = (req, res) => {
+  //req.body.orderId aamra front end theke nibo
+  Order.update({ _id: req.body.orderId }),
+    { $set: { status: req.body.status } },
+    (err, orders) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(orders);
+    };
 };
--- SellMate/routes/order.js
+++ SellMate/routes/order.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders } = require("../controllers/order");
+const {
+  create,
+  listOrders,
+  getStatusValues,
+  updateOrderStatus,
+  orderById,
+} = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.get(
@@ -13,7 +19,22 @@ router.get(
   decreaseQuantity,
   create
 ),
-  router.post("/order/list/:userId", requireSignin, isAdmin, listOrders);
+  router.get("/order/list/:userId", requireSignin, isAdmin, listOrders);
+
+router.get(
+  "/order/status-values/:userId",
+  requireSignin,
+  isAdmin,
+  getStatusValues
+);
+
+router.put(
+  "/order/:orderId/status/:userId",
+  requireSignin,
+  isAdmin,
+  updateOrderStatus
+);
 router.param("userId", userById);
+router.param("orderId", orderById);
 
 module.exports = router;
